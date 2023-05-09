86185ca223534696d6c4ffd95889d25a9a789cde
maximsv <maximsv1701@gmail.com>
2023-05-09 16:18:12 +0300

    sliders_low

--- js/commons.js
+++ js/commons.js
@@ -1,75 +1,59 @@
 $(document).ready(function () {
   $(".slider").slick({
-    arrows: true,
+    arrows: false,
     dots: true,
-    slidesToShow: 3,
+    slidesToShow: 4,
+    slidesToScroll: 1,
     autoplay: true,
-    speed: 1000,
-    autoplaySpeed: 800,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 550,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
+    autoplaySpeed: 2000,
   });
+  // $(".slider").slick({
+  //   arrows: false,
+  //   dots: true,
+  //   slidesToShow: 4,
+  //   autoplay: true,
+  //   speed: 1000,
+  //   autoplaySpeed: 800,
+  //   responsive: [
+  //     {
+  //       breakpoint: 768,
+  //       settings: {
+  //         slidesToShow: 2,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 550,
+  //       settings: {
+  //         slidesToShow: 1,
+  //       },
+  //     },
+  //   ],
+  // });
 });
 
-// $(document).ready(function () {
-//   $(".slider").slick({
-//     // arrows:true,
-//     // dots:true,
-//     // slidesToShow:3,
-//     // autoplay:true,
-//     // speed:1000,
-//     // autoplaySpeed:800,
-//     // responsive:[
-//     //   {
-//     //     breakpoint: 768,
-//     //     settings: {
-//     //       slidesToShow:2
-//     //     }
-//     //   },
-//     //   {
-//     //     breakpoint: 550,
-//     //     settings: {
-//     //       slidesToShow:1
-//     //     }
-//     //   }
-//     // ]
-//   });
-
-//   $(".top").click(function () {
-//     $("html, body").animate({ scrollTop: 0 }, "slow");
-//     return false;
-//   });
+// $(".top").click(function () {
+//   $("html, body").animate({ scrollTop: 0 }, "slow");
+//   return false;
+// });
 
 //   //Аякс отправка форм
 //   //Документация: http://api.jquery.com/jquery.ajax/
-//   $(".forms").submit(function () {
-//     $.ajax({
-//       type: "POST",
-//       url: "mail.php",
-//       data: $(this).serialize(),
-//     }).done(function () {
-//       alert("Спасибо за заявку!");
-//       setTimeout(function () {
-//         $.magnificPopup.close();
-//         $(".forms").trigger("reset");
-//       }, 1000);
-//     });
-//     return false;
+// $(".forms").submit(function () {
+//   $.ajax({
+//     type: "POST",
+//     url: "mail.php",
+//     data: $(this).serialize(),
+//   }).done(function () {
+//     alert("Спасибо за заявку!");
+//     setTimeout(function () {
+//       $.magnificPopup.close();
+//       $(".forms").trigger("reset");
+//     }, 1000);
 //   });
+//   return false;
+// });
 
-//   //Chrome Smooth Scroll
+//Chrome Smooth Scroll
 //   try {
 //     $.browserSelector();
 //     if ($("html").hasClass("chrome")) {
