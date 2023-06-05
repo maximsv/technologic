e8fd21355cd4232e7b74e36dcb7d946c1bcb5bba
maximsv <maximsv1701@gmail.com>
2023-06-05 19:08:25 +0300

    news

--- mail.php
+++ mail.php
@@ -7,12 +7,14 @@ $subject   = "Новая заявка с сайта \"$sitename\"";
 
 $name = trim($_POST["name"]);
 $phone = trim($_POST["phone"]);
+$mail = trim($_POST["mail"]);
 // $formname = trim($_POST["formname"]);
 
 $message = "
 Форма: $formname <br>
 Имя: $name <br>
-Телефон: $phone
+Телефон: $phone <br>
+Почта: $mail
 ";
 
 mail($recepient, $subject, $message, "From: $frm_name <$email>" . "\r\n" . "Reply-To: $email" . "\r\n" . "X-Mailer: PHP/" . phpversion() . "\r\n" . "Content-type: text/html; charset=\"utf-8\"");
